cbd73438906452d3af96a457c62760638ead88cb
DiegoArturoGuzman <diegoarturo170@gmail.com>
2025-09-11 21:07:04 -0600

    Primer Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'basesAngular01';
+  title = 'Diego Guzman';
+
+  duplicarNumero(valor: number): number {
+    return valor * 2;
+  }
+
+  pelicula={
+    titulo:"El padrino",
+    anio:1972,
+    genero:"Crimen/Drama"
+  }
 }
